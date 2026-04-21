b9db264ccad0acfe051d7f4da0c1b33b95ad249c
zwahab <zainabwahab.eth@gmail.com>
2026-04-21 12:00:56 +0100

    initial ascii art web

--- ascii-art-web/main.go
+++ ascii-art-web/main.go
@@ -1,88 +1,12 @@
 package main
 
 import (
-	"ascii-art-web/operations"
-	"html/template"
-	"log"
+	"ascii-art-web/controller"
 	"net/http"
-	"strings"
 )
 
-type PageData struct {
-	Input  string
-	Banner string
-	Result string
-	Error  string
-}
-
-// var tmpl = template.Must(template.ParseFiles("templates/index.html"))
-var tmpl = template.Must(template.ParseFiles("templates/index.html", "templates/notFound.html"))
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	//Make sure path is "/". Check if path is not "/"
-	if r.URL.Path != "/" {
-		//Write status 404
-		w.WriteHeader(http.StatusNotFound)
-
-		//Load error page template and return Error
-		err := tmpl.ExecuteTemplate(w, "notFound.html", PageData{Error: "Page Not Found"})
-
-		//If there is template loading error
-		if err != nil {
-			log.Println("template error:", err)
-		}
-
-		return
-	}
-
-	//If the correct route "/" is entered print the index page
-	if err := tmpl.ExecuteTemplate(w, "index.html", PageData{}); err != nil {
-    log.Println("template error:", err)
-    return
-}
-}
-
-func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-
-	text := r.FormValue("text")
-	banner := r.FormValue("banner")
-
-	// 400 — bad input from the user
-	if text == "" || banner == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		tmpl.Execute(w, PageData{Error: "text and banner are required"})
-		return
-	}
-
-	bannerLoc := banner + ".txt"
-
-	// read banner
-	data, err := operations.ReadTextFile(bannerLoc)
-
-	if err != nil {
-		// 404 — banner file wasn't found
-		w.WriteHeader(http.StatusNotFound)
-		tmpl.Execute(w, PageData{Error: "banner file not found"})
-		return
-	}
-
-	inputSlice := strings.Split(text, "\\n")
-
-	//Call ascii art logic
-	result, err := operations.AsciiArt(inputSlice, data)
-	if err != nil {
-		// 500 — template itself is broken/missing
-		w.WriteHeader(http.StatusInternalServerError)
-		tmpl.Execute(w, PageData{Error: "Something went wrong"})
-		return
-	}
-
-	tmpl.Execute(w, PageData{Input: text, Banner: banner, Result: result})
-}
-
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/ascii-art", asciiArtHandler)
+	http.HandleFunc("/", controller.IndexHandler)
+	http.HandleFunc("/ascii-art", controller.AsciiArtHandler)
 	http.ListenAndServe(":8080", nil)
 }
